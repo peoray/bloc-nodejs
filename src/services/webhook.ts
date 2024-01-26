d9b4160dffdd536d61c86309ac82d7b31971f9d2
peoray <peoray@yahoo.com>
2024-01-26 15:18:47 +0100

    docs: Add jsdocs to webhook methods class

--- src/services/webhook.ts
+++ src/services/webhook.ts
@@ -1,23 +1,32 @@
 import { HTTPCore } from '../api'
 import { ISetWebhook, IWebhookResponse } from '../types'
-import {} from '../types'
 
 /**
- * Class representing Customer operations that extends HTTPCore.
+ * Class representing operations related to Webhooks, extending HTTPCore.
  */
-export class Webhhok extends HTTPCore {
+export class Webhook extends HTTPCore {
   /**
-   * Creates an instance of the Webhhok class.
+   * Creates an instance of the Webhook class.
    * @param {string} secretKey - The secret key for authentication.
    * @param {string} publicKey - The public key for authentication.
    */
   constructor(public secretKey: string, public publicKey: string) {
     super(secretKey, publicKey)
   }
+
+  /**
+   * Sets a webhook using the provided data.
+   * @param {ISetWebhook} data - The data to set the webhook.
+   * @returns {Promise<IWebhookResponse>} A promise that resolves to the webhook response.
+   */
   public async setWebhook(data: ISetWebhook): Promise<IWebhookResponse> {
     return this.post<IWebhookResponse>(`/webhooks`, data)
   }
 
+  /**
+   * Retrieves the current webhook configuration.
+   * @returns {Promise<IWebhookResponse>} A promise that resolves to the webhook response.
+   */
   public async getWebhook(): Promise<IWebhookResponse> {
     return this.get<IWebhookResponse>(`/webhooks`)
   }
