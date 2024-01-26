5c92f50b1e75b3534947a4b272aa850b257f1d3e
peoray <peoray@yahoo.com>
2024-01-26 16:18:43 +0100

    docs: Add jsdocs to simulation method class

--- src/services/simulation.ts
+++ src/services/simulation.ts
@@ -6,11 +6,11 @@ import {
 } from '../types'
 
 /**
- * Class representing operations related to Webhooks, extending HTTPCore.
+ * Class representing operations related to Simulations, extending HTTPCore.
  */
 export class Simulation extends HTTPCore {
   /**
-   * Creates an instance of the Webhook class.
+   * Creates an instance of the Simulation class.
    * @param {string} secretKey - The secret key for authentication.
    * @param {string} publicKey - The public key for authentication.
    */
@@ -18,12 +18,22 @@ export class Simulation extends HTTPCore {
     super(secretKey, publicKey)
   }
 
+  /**
+   * Simulates crediting an account using the provided data.
+   * @param {ISimulationAccount} data - The data to simulate the credit operation.
+   * @returns {Promise<ICreditAccountResponse>} A promise that resolves to the credit account response.
+   */
   public async creditAccount(
     data: ISimulationAccount
   ): Promise<ICreditAccountResponse> {
     return this.post<ICreditAccountResponse>(`/accounts/credit/manual`, data)
   }
 
+  /**
+   * Simulates debiting an account using the provided data.
+   * @param {ISimulationAccount} data - The data to simulate the debit operation.
+   * @returns {Promise<IDebitAccountResponse>} A promise that resolves to the debit account response.
+   */
   public async debitAccount(
     data: ISimulationAccount
   ): Promise<IDebitAccountResponse> {
