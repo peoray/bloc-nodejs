1b894c5e1d1c7d32b599612f4ad1f8933a4488c3
peoray <peoray@yahoo.com>
2024-01-28 00:16:15 +0100

    docs: Add jsdocs to wallets method class

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -8,11 +8,11 @@ import {
 } from '../types'
 
 /**
- * Class representing Customer operations that extends HTTPCore.
+ * Class representing operations related to Wallets, extending HTTPCore.
  */
 export class Wallet extends HTTPCore {
   /**
-   * Creates an instance of the Beneficiary class.
+   * Creates an instance of the Wallet class.
    * @param {string} secretKey - The secret key for authentication.
    * @param {string} publicKey - The public key for authentication.
    */
@@ -20,18 +20,37 @@ export class Wallet extends HTTPCore {
     super(secretKey, publicKey)
   }
 
+  /**
+   * Creates a new wallet using the provided data.
+   * @param {IWallet} data - The data to create the wallet.
+   * @returns {Promise<IWalletResponse>} A promise that resolves to the wallet creation response.
+   */
   public async createWallet(data: IWallet): Promise<IWalletResponse> {
     return this.post<IWalletResponse>(`/wallets`, data)
   }
 
+  /**
+   * Retrieves a list of wallets.
+   * @returns {Promise<IGetWalletResponse>} A promise that resolves to the list of wallets response.
+   */
   public async getWallets(): Promise<IGetWalletResponse> {
     return this.get<IGetWalletResponse>(`/wallets`)
   }
 
+  /**
+   * Retrieves a wallet by its ID.
+   * @param {string} walletID - The ID of the wallet to retrieve.
+   * @returns {Promise<IWalletResponse>} A promise that resolves to the wallet response.
+   */
   public async getWalletById(walletID: string): Promise<IWalletResponse> {
     return this.get<IWalletResponse>(`/wallets/${walletID}`)
   }
 
+  /**
+   * Retrieves the wallets associated with a specific customer.
+   * @param {string} customerID - The ID of the customer to retrieve wallets for.
+   * @returns {Promise<IGetCustomerWalletResponse>} A promise that resolves to the customer's wallets response.
+   */
   public async getCustomerWallets(
     customerID: string
   ): Promise<IGetCustomerWalletResponse> {
@@ -40,6 +59,11 @@ export class Wallet extends HTTPCore {
     )
   }
 
+  /**
+   * Simulates debiting a wallet using the provided data.
+   * @param {IDebitWallet} data - The data to simulate the debit operation.
+   * @returns {Promise<IGetCustomerWalletResponse>} A promise that resolves to the customer's wallets response after the debit operation.
+   */
   public async debitWallet(
     data: IDebitWallet
   ): Promise<IGetCustomerWalletResponse> {
