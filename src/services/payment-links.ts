9fb557abbc0933f448278b61ee0087039028e893
peoray <peoray@yahoo.com>
2024-01-29 14:52:45 +0100

    docs: Add jsdocs to payment-links method class

--- src/services/payment-links.ts
+++ src/services/payment-links.ts
@@ -9,7 +9,7 @@ import {
 } from '../types'
 
 /**
- * Class representing operations related to PaymentLinks, extending HTTPCore.
+ * Class representing operations related to Payment Links, extending HTTPCore.
  */
 export class PaymentLinks extends HTTPCore {
   /**
@@ -21,22 +21,42 @@ export class PaymentLinks extends HTTPCore {
     super(secretKey, publicKey)
   }
 
+  /**
+   * Creates a new payment link using the provided data.
+   * @param {ICreatePaymentLinkRequest} data - The data to create the payment link.
+   * @returns {Promise<IPaymentLinkResponse>} A promise that resolves to the payment link creation response.
+   */
   public async createPaymentLink(
     data: ICreatePaymentLinkRequest
   ): Promise<IPaymentLinkResponse> {
     return this.post<IPaymentLinkResponse>(`/paymentlinks`, data)
   }
 
+  /**
+   * Retrieves a list of payment links.
+   * @returns {Promise<IGetPaymentLinksResponse>} A promise that resolves to the list of payment links response.
+   */
   public async getPaymentLinks(): Promise<IGetPaymentLinksResponse> {
     return this.get<IGetPaymentLinksResponse>(`/paymentlinks`)
   }
 
+  /**
+   * Retrieves a payment link by its ID.
+   * @param {string} linkId - The ID of the payment link to retrieve.
+   * @returns {Promise<IPaymentLinkResponse>} A promise that resolves to the payment link response.
+   */
   public async getPaymentLinkById(
     linkId: string
   ): Promise<IPaymentLinkResponse> {
     return this.get<IPaymentLinkResponse>(`/paymentlinks/${linkId}`)
   }
 
+  /**
+   * Edits an existing payment link using the provided data.
+   * @param {string} linkId - The ID of the payment link to edit.
+   * @param {IEditPaymentLinkRequest} data - The data to edit the payment link.
+   * @returns {Promise<IPaymentLinkResponse>} A promise that resolves to the edited payment link response.
+   */
   public async editPaymentLink(
     linkId: string,
     data: IEditPaymentLinkRequest
@@ -44,6 +64,11 @@ export class PaymentLinks extends HTTPCore {
     return this.put<IPaymentLinkResponse>(`/paymentlinks/${linkId}`, data)
   }
 
+  /**
+   * Deletes one or more payment links using the provided data.
+   * @param {IDeletePaymentLinksRequest} data - The data to delete the payment links.
+   * @returns {Promise<IDeletePaymentLinksResponse>} A promise that resolves to the payment link deletion response.
+   */
   public async deletePaymentLink(
     data: IDeletePaymentLinksRequest
   ): Promise<IDeletePaymentLinksResponse> {
