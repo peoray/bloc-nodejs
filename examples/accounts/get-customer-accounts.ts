5a56372ef98e08de3442f75a1322f284877b6e52
peoray <peoray@yahoo.com>
2024-01-29 22:30:58 +0100

    docs: Add code example to get customer accounts

---
+++ examples/accounts/get-customer-accounts.ts
@@ -0,0 +1,14 @@
+import { Bloc, ICreateCollectionAccountRequest } from '../../dist'
+
+const bloc = new Bloc('secret-keyasfasfbahfb', 'public-key')
+
+const getCustomerAccounts = async () => {
+  try {
+    const response = await bloc.getCustomerAccounts('customer-id')
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getCustomerAccounts()
