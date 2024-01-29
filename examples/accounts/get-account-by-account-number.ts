a06f56f0ee30dedda75f9bbedd90622877d199f4
peoray <peoray@yahoo.com>
2024-01-29 22:29:47 +0100

    docs: Add code example to get account by account number

---
+++ examples/accounts/get-account-by-account-number.ts
@@ -0,0 +1,14 @@
+import { Bloc, ICreateCollectionAccountRequest } from '../../dist'
+
+const bloc = new Bloc('secret-keyasfasfbahfb', 'public-key')
+
+const getAccountByAccountNumber = async () => {
+  try {
+    const response = await bloc.getAccountByAccountNumber('account-id')
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getAccountByAccountNumber()
